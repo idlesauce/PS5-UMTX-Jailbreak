e83aba17c626a2044e493524d352227e50e297d7
idlesauce <148508202+idlesauce@users.noreply.github.com>
2024-11-02 10:57:24 +0100

    Update Byepervisor Now also supports 1.00-1.05+2.70

--- document/en/ps5/payload_map.js
+++ document/en/ps5/payload_map.js
@@ -46,9 +46,9 @@ const payload_map = [
         fileName: "byepervisor.elf",
         author: "SpecterDev, ChendoChap, flatz, fail0verflow, Znullptr, kiwidog, sleirsgoevy, EchoStretch",
         projectSource: "https://github.com/EchoStretch/Byepervisor",
-        binarySource: "https://github.com/EchoStretch/Byepervisor/actions/runs/11545292602",
-        version: "4655f86",
-        supportedFirmwares: ["1.12", "1.14", "2.00", "2.20", "2.25", "2.26", "2.30", "2.50"],
+        binarySource: "https://github.com/EchoStretch/Byepervisor/actions/runs/11638840901",
+        version: "3e28421",
+        supportedFirmwares: ["1.00", "1.01", "1.02", "1.05", "1.12", "1.14", "2.00", "2.20", "2.25", "2.26", "2.30", "2.50", "2.70"],
         toPort: 9021
     },
     {
