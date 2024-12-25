ce102fb07abf3de74f45681e4527d620bc3a0530
idlesauce <148508202+idlesauce@users.noreply.github.com>
2024-12-25 21:24:10 +0000

    Update ps5debug by @GoldHEN

--- document/en/ps5/payload_map.js
+++ document/en/ps5/payload_map.js
@@ -137,12 +137,13 @@ const payload_map = [
     {
         displayTitle: "ps5debug",
         description: "Debugger (Experimental beta)",
-        fileName: "ps5debug.elf",
+        fileName: "ps5debug_v1.0b2.elf",
         author: "SiSTR0, ctn123",
         projectSource: "https://github.com/GoldHEN/ps5debug",
-        binarySource: "https://github.com/GoldHEN/ps5debug/releases/download/1.0b1/ps5debug_v1.0b1.7z",
-        version: "1.0b1",
-        supportedFirmwares: ["3.", "4."]
+        binarySource: "https://github.com/GoldHEN/ps5debug/releases/download/1.0b2/ps5debug_v1.0b2.elf",
+        version: "1.0b2",
+        supportedFirmwares: ["3.", "4.", "5."],
+        toPort: 9021
     },
     {
         displayTitle: "ps5debug",
